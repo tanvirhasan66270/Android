e426fa9e001768c54fc5e5506986d4b6d629dce9
Md. Tanvir <tanvirhasan66270@gmail.com>
2026-07-29 19:10:17 +0600

    add

--- SCM/app/src/main/java/com/project/scm/Dashboard_Activity.java
+++ SCM/app/src/main/java/com/project/scm/Dashboard_Activity.java
@@ -1,8 +1,6 @@
 package com.project.scm;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,19 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Login_Activity extends AppCompatActivity {
+public class Dashboard_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-
-        Button btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(Login_Activity.this, Dashboard_Activity.class);
-            startActivity(intent);
-        });
+        setContentView(R.layout.activity_dashboard);
 
     }
 }
